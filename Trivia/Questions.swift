fc034ae1fa39346822efbc7271ae7d5fb3e96a27
Katgirl_J <55986@Scps.net>
2026-09-16 23:35:57 -0400

    Commiting

--- Trivia/Questions.swift
+++ Trivia/Questions.swift
@@ -5,4 +5,25 @@
 //  Created by Katrinna Jones on 9/16/26.
 //
 
-import Foundation
+import UIKit
+
+struct QuestionsandAnswers{
+    let question: String
+    let questionType: String
+    let questionSubType: String
+    let answers: [String: Bool]
+}
+
+class Questions {
+    
+    static let questionsAsked: [QuestionsandAnswers] = [
+        QuestionsandAnswers(
+            question: "What planet is closet to the sun?", questionType: "Science", questionSubType: "Astronomy", answers: ["Venus": false, "Mercury": true, "Mars": false, "Earth": false]),
+        QuestionsandAnswers(question: "What does the CPU stand for?", questionType: "Computer Science", questionSubType: "Hardware", answers: ["Central Processing Unit": true, "Computer Personal Unit": false, "Central Program Utility": false, "Core Processing Unit": false]),
+        QuestionsandAnswers(question: "Which language has the most native speakers?", questionType: "World", questionSubType: "Language", answers: ["English": false, "Hindi": false, "Manderian": true, "Spanish": false]),
+        QuestionsandAnswers(question: "What is the world's longest river", questionType: "Geography", questionSubType: "Rivers", answers: ["Amazon": false, "Mississippi": false, "Yangtze": false, "Nile": true]),
+        QuestionsandAnswers(question: "How many stripes are on the US flag?", questionType: "US History", questionSubType: "Flags", answers: ["11": false, "15": false, "13": true, "50": false])
+    ]
+    
+}
+
--- ViewController.swift
+++ ViewController.swift
@@ -7,15 +7,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        displayCurrentQuestion()
         // Do any additional setup after loading the view.
     }
     
-
+    var questionIndex = 0
+    var score = 0
+    
+    // For question label
+    @IBOutlet weak var questionNumLabel: UILabel!
+    // For Type and Subtype label
+    @IBOutlet weak var typeWithSubLabel: UILabel!
+    // For the question itself
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet var answerButtons: [UIButton]!
+    
+    // For the question being displayed itself
+    
+    var correctAnswers = 0
+    //If any button is pressed
+    
+    
+    func displayCurrentQuestion(){
+        let allQuestions = Questions.questionsAsked
+        
+        guard questionIndex < allQuestions.count else{return}
+        
+        guard let numLabel = questionNumLabel else{return}
+        guard let types = typeWithSubLabel else{return}
+        guard let questions = questionLabel else{return}
+        guard let answerButtons = answerButtons else{return}
+        
+        
+        
+        
+        let currentQuestion = allQuestions[questionIndex]
+        
+        numLabel.text = "Question \(questionIndex+1) / \(allQuestions.count)"
+        types.text = "\(currentQuestion.questionType) : \(currentQuestion.questionSubType)"
+        questions.text = currentQuestion.question
+        
+        let answerChoices = Array(currentQuestion.answers.keys)
+        
+        for (index, button) in answerButtons.enumerated(){
+            if(index < currentQuestion.answers.count){
+                button.isHidden = false
+                button.setTitle(answerChoices[index], for: .normal)
+            }
+            else{
+                button.isHidden = true
+            }
+        }
+    }
+    @IBAction func buttonPressed(_ sender: UIButton){
+        let allQuestions = Questions.questionsAsked
+        guard questionIndex < allQuestions.count else { return }
+        
+        let currentQuestion = allQuestions[questionIndex]
+        let answerChoices = Array(currentQuestion.answers.keys)
+        
+        guard let buttonIndex = answerButtons.firstIndex(of: sender),
+              buttonIndex < answerChoices.count else { return }
+        
+        let selectedAnswer = answerChoices[buttonIndex]
+        
+        if let isCorrect = currentQuestion.answers[selectedAnswer], isCorrect{
+            score+=1
+        }
+        questionIndex+=1
+        
+        if questionIndex < allQuestions.count{
+            displayCurrentQuestion()
+        }
+        else{
+            
+        }
+    }
+    
+    func showTotalScore(){
+        questionLabel.text = "Congraduations your final score is \(score)/\(Questions.questionsAsked.count)"
+    }
+    /*public func checkingAnswer(_ currentAnswers: Questions.questionsAsked.answers) -> Bool{
+        for (answer in currentAnswers.values){
+            if(answer){
+                score+=1
+                return true
+            }
+        }
+        return false
+    }*/
+            
+    //}
     /*
     // MARK: - Navigation
 
@@ -25,5 +113,9 @@ class ViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
+
+
+    
+
+
